8858339a223315441c77e5013439d936cbf63e42
Sayaka Ishikawa <166687596+kawa3saya@users.noreply.github.com>
2026-09-16 14:25:56 +0100

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        const res = new Map();
+        for(let i=0; i<nums.length; i++){
+            let expectedNum = target - nums[i];
+            if(res.has(expectedNum)){
+                return [res.get(expectedNum), i];
+            }else{
+                res.set(nums[i], i);
+            }
+        }
+    }
+}
