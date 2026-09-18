04750626151a01b8c2b968b4dafdee8c7d2d09d9
Sayaka Ishikawa <166687596+kawa3saya@users.noreply.github.com>
2026-09-18 10:45:17 +0100

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        const arr = s.replace(/[^a-zA-Z0-9]/g,"").toLowerCase().split("")
+        console.log(arr)
+        let last = arr.length -1
+        let start = 0
+        while(start < last){
+            if(arr[start] === arr[last]){
+                start ++
+                last --
+            }else{
+                return false
+            }
+        }
+        return true
+    }
+}
